5b44f73669992822bb0a9fee6e6c47ec0820fe81
Michael Watchmaker <watchmmb@miamioh.edu>
2019-07-21 10:27:43 -0700

    merge placeholder to feedData

--- src/index.js
+++ src/index.js
@@ -6,9 +6,47 @@ import Card from "./Card";
 class App extends React.Component {
 
     constructor(props){
+
         super(props);
 
-        this.feedData = [];
+        let placeholderData = (
+            <div>
+                <Card>
+                    <Post
+                        color = "mediumseagreen"
+                        name = "Michael Watchmaker"
+                        handle = "michael"
+                        content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus nisi sit amet dolor viverra sollicitudin. Mauris et ullamcorper leo. Fusce egestas turpis sit amet vulputate egestas. Sed lacinia dapibus mi sed consectetur. Donec pharetra vitae ex eu pretium. Quisque vitae tincidunt leo. In pharetra purus lectus, nec tempus sapien tempor nec."
+                    />
+                </Card>
+                <Card>
+                    <Post
+                        color = "coral"
+                        name = "Jay Lucien"
+                        handle = "jay"
+                        content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    />
+                </Card>
+                <Card>
+                    <Post
+                        color = "mediumpurple"
+                        name = "Ethan Faye"
+                        handle = "ethan"
+                        content = "HeyDoctor this is Ethan."
+                    />
+                </Card>
+                <Card>
+                    <Post
+                        color = "lightskyblue"
+                        name = "Mr. Krabs"
+                        handle = "krabby"
+                        content = "That smell. A kind of smelly smell. A smelly smell that smells...smelly. ANCHOVIES!!!!"
+                    />
+                </Card>
+            </div>
+        );
+
+        this.feedData = [placeholderData, ];
 
         this.state = {
             showData: this.feedData,
@@ -77,38 +115,6 @@ class App extends React.Component {
                     <div>
                         {this.feedData}
                     </div>
-                    <Card>
-                        <Post
-                            color = "mediumseagreen"
-                            name = "Michael Watchmaker"
-                            handle = "michael"
-                            content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus nisi sit amet dolor viverra sollicitudin. Mauris et ullamcorper leo. Fusce egestas turpis sit amet vulputate egestas. Sed lacinia dapibus mi sed consectetur. Donec pharetra vitae ex eu pretium. Quisque vitae tincidunt leo. In pharetra purus lectus, nec tempus sapien tempor nec."
-                        />
-                    </Card>
-                    <Card>
-                        <Post
-                            color = "coral"
-                            name = "Jay Lucien"
-                            handle = "jay"
-                            content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                        />
-                    </Card>
-                    <Card>
-                        <Post
-                            color = "mediumpurple"
-                            name = "Ethan Faye"
-                            handle = "ethan"
-                            content = "HeyDoctor this is Ethan."
-                        />
-                    </Card>
-                    <Card>
-                        <Post
-                            color = "lightskyblue"
-                            name = "Mr. Krabs"
-                            handle = "krabby"
-                            content = "That smell. A kind of smelly smell. A smelly smell that smells...smelly. ANCHOVIES!!!!"
-                        />
-                    </Card>
                     <p className="feed-end">End of the feed</p>
                 </div>
             </div>
